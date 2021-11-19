51f299d232f0e4564368dc32264b94b4c1c02df4
David O Mahony <dave.omahony@betwaygroup.com>
2021-11-19 14:13:29 +0000

    Update gitignore

---
+++ SharesInformationProvider.Models/BasicSharesQueryInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SharesInformationProvider.Models
+{
+    public class BasicSharesQueryInfo
+    {
+        public string ListingName { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public DateTimeOffset EndDate { get; set; }
+
+        public int TimeIncrement { get; set; }
+    }
+}
---
+++ SharesInformationProvider.Models/BasicSharesQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SharesInformationProvider.Models
+{
+    public class BasicSharesQueryResponse
+    {
+        public string ListingName { get; set; }
+
+        public Dictionary<string, float> Results { get; set; }
+    }
+}
